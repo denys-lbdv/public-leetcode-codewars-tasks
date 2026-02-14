92e036c66c068d1cde7f3cbf469555fb1a37e707
Denys Lebedev <denis_bc@ukr.net>
2026-02-14 13:17:29 +0200

    one more resolved

---
+++ tests/leetcode/2026/feb/13-task.spec.ts
@@ -0,0 +1,11 @@
+import { majorityElement } from "../../../../src/leetcode/2026/feb/13-task/14-feb-2025-task_13";
+import test, { expect } from "@playwright/test";
+
+test.describe("Majority Element", async () => {
+  test("case 1", async () => {
+    expect(majorityElement([3, 2, 3])).toBe(3);
+  });
+  test("case 2", async () => {
+    expect(majorityElement([2, 2, 1, 1, 1, 2, 2])).toBe(2);
+  });
+});
