1e09751b920d8d6a7b7c5fa6b864d36982b82db1
Denys Lebedev <denis_bc@ukr.net>
2026-03-04 08:18:36 +0200

    one task resolved

---
+++ src/leetcode/2026/30-days-of-js-challenge/task-13_04-March/task-13_04-March.ts
@@ -0,0 +1,5 @@
+export async function sleep(millis: number): Promise<void> {
+return new Promise<void>((resolve) => {
+    setTimeout(()=>resolve(), millis);
+  });
+}
---
+++ tests/leetcode/2026/30-days-of-js-challenge/task-13_04-March.spec.ts
@@ -0,0 +1,11 @@
+import { sleep } from "../../../../src/leetcode/2026/30-days-of-js-challenge/task-13_04-March/task-13_04-March";
+import test, { expect } from "@playwright/test";
+
+test.describe("Sleep", async () => {
+  test("Sleep case 1", async () => {
+    let startTime = Date.now();
+    await sleep(100);
+    let endTime = Date.now();
+    expect(endTime-startTime).toBeGreaterThan(100);
+  });
+});
